d40a783e62cad353a9235a6dcc8ae849566b8bd2
carlmaeckler <carl.maeckler@student.unisg.ch>
2020-08-25 17:36:29 +0200

    new column: name, lifetime, country

--- data_processing.py
+++ data_processing.py
@@ -1,16 +1,45 @@
-import pandas as pd
+iimport pandas as pd
+pd.set_option('display.max_rows', n)
 
-data = pd.read_csv('paintings_df.csv')
+data_csv = pd.read_csv('paintings_df.csv')
+data_csv
 
-def artist_cleaning(column_name):
-    data.loc[:,column_name] = data[column_name].str.replace("\r\n ", "")
-    return data
+data = data_csv.copy()
 
-name = 'Friedrich Preller D A (1804-1878) Germany'
+# Cleaning
 
+## Functions
+
+def clean_artist(name):
+    name = name.replace("\r\n ", "")
+    name = name.replace("United States of America", "USA")
+    name = name.replace("...", ".")
+    return name
+
+# getting country column
 def get_country(name):
-    return name.split(' ')[-1]
+    splitted_name = name.split(' ')[-1]
+    return splitted_name
 
-data['Country'] = data.artist.apply(get_country)
+# getting lifetime column
+def get_lifetime(name):
+    lifetime = name.split(' ')[-2]
+    lifetime = lifetime.strip('()')
+    return lifetime
 
-artist_cleaning('artist')
+# getting name column
+def get_name(column):
+    newlist= column.split(' ')[0:-2]
+    #newlist= newlist.str.replace()
+    return ' '.join(newlist).strip()
+
+# getting birth date
+def get_birthdate(date):
+    splitdate= date.split('-')[0]
+    return splitdate
+
+data['artist'] = data.artist.apply(clean_artist)
+data['country'] = data.artist.apply(get_country)
+data['lifetime'] = data.artist.apply(get_lifetime)
+data['name'] = data.artist.apply(get_name)
+data['birth_year'] = data.lifetime.apply(get_birthdate)
